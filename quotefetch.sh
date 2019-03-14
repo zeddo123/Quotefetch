7f94cc5a85cd175c724fe5454e7a71ac68c62e09
zeddo123 <mouhadrissi9@gmail.com>
2019-03-14 19:29:50 +0100

    bug fix

--- quotefetch.sh
+++ quotefetch.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 python fetch.py
+
+b=$'\033[m'
+y=$'\033[1;33m'
+
+cpu=$(cat /proc/cpuinfo | grep "model name" | cut -d ' ' -f5 | head -1)
+shell=$(basename $SHELL)
 quote=$(<quote.txt)
+distro=$(uname -r | cut -d '-' -f3)
+
 cat<< END
 
-	$quote
-
+$quote
+	$y SHELL:$shell$b 	$y Distro:$distro$b
+	$y CPU:$cpu$b
 END
