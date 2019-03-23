f3461525961bd19c25f50e1ffa0fdc7d4c7ae538
zeddo123 <mouhadrissi9@gmail.com>
2019-03-23 23:28:07 +0100

    changed to another 'API',Added display user name

--- fetch.py
+++ fetch.py
@@ -1,35 +1,58 @@
-#!/usr/bin/env python
-import html
-import requests
+#!/usr/bin/env python3
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
 import re
 
 def format_str(l):
-	max_line = len(max(l))
+	max_line = max([len(i) for i in l])
 
 	for i in range(len(l)):
 		while len(l[i]) < max_line:
-			l[i] = " " + l[i] + " "
+			l[i] = ' ' + l[i] + ' ' # Format the lines to be equale
 
-line_len = 7 #The maximum nomber of words in one line
-text_color = ''
+def get_content(url):
+	try:
+		with closing(get(url, stream=True)) as resp:
+			if is_good_response(resp):
+				return resp.content
+			else:
+				return None
+
+	except RequestException as e:
+		print('Error during requests')
+		return None# Get the quote from the web site
+
+def is_good_response(resp):
+	content_type = resp.headers['Content-Type'].lower()
+	return(resp.status_code == 200 
+			and content_type is not None 
+			and content_type.find('html') > -1)
+
+line_len = 8 #The maximum number of words in one line
+text_color = '' # The Color of the quote (Default : White)
 type_of_quote = '“”' # '«»'
-quote_color = '1;33'
+quote_color = '1;33' # The color of the quote " ' "
 file_name = 'quote.txt'
 
-url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+url = 'https://www.quotedb.com/quote/quote.php?action=random_quote'
 
 try:
-	raw_file = requests.get(url).json()
+	raw = get_content(url).decode()
 except:
 	print('Connection Error')
 	exit()
 
-content = re.sub(r'<.*?>', '',raw_file[0]['content'])
-while '\n' in content:
-	content = content.replace('\n','')
+quote = re.findall(r'document.write\(\'(.*)\'\);', raw)
 
-content = html.unescape(content)
-author = raw_file[0]['title']
+quote[1] = re.sub(r'<.*?>','',quote[1])
+quote[0] = re.sub(r'<.*?>','',quote[0])
+
+quote[1] = quote[1][16:]
+
+
+content = quote[0]
+author = quote[1]
 
 words = content.split(' ')
 lines = []
@@ -39,14 +62,16 @@ while words != []:
 	words = words[line_len:]
 	lines.append(line)
 
+
 format_str(lines)
+
 lines = [i + '\n' for i in lines]
 file = open(file_name,'w')
 
 file.write(f'\033[{quote_color}m{type_of_quote[0]} \033[{text_color}m')
 for line in lines:
 	file.write(line)
-file.write(f'\033[{quote_color}m{type_of_quote[1]}\033[{text_color}m {author}\n')
+file.write(f'{(len(max(lines))-len(author))* " "} \033[{quote_color}m{type_of_quote[1]}\033[{text_color}m {author}\n')
 
 file.close()
 
--- quotefetch.sh
+++ quotefetch.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-python fetch.py
+python3 fetch.py
 
 b=$'\033[m'
 y=$'\033[1;33m'
 
 cpu=$(cat /proc/cpuinfo | grep "model name" | cut -d ' ' -f5 | head -1)
-shell=$(basename $SHELL)
-quote=$(<quote.txt)
-distro=$(uname -r | cut -d '-' -f3)
 
+shell=$(basename $SHELL)
+
+quote=$(<quote.txt)
+
+distro=$(hostnamectl | cut -d: -f2 | sed -n "6p")
+#$(uname -r | cut -d '-' -f3)
+
+user=$USER
 cat<< END
 
 $quote
 $y SHELL:$shell$b 	$y Distro:$distro$b
-$y CPU:$cpu$b
+$y User:$user$b 	$y CPU:$cpu    
 END
